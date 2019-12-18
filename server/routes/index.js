06b4d3a8b0483ad44920fae9e7cda5a5f7978c1f
Israel Martinez <imartinez@carecloud.com>
2019-12-18 08:57:35 -0500

    Implement twitter search

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,32 @@
 ﻿const express = require("express");
-const client = require('../twitter')()
-console.log("*".repeat(20), client);
+const Twitter = require('twitter');
+require('dotenv').config()
 
 const router = express.Router();
+let obj = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  bearer_token: process.env.BEARER_TOKEN
+}
 
-router.get('/tester', (req,res) => {
-    client.get('search/tweets', {q: '@fi_ramen OR #fi_ramen'}, function(error, tweets, response) {
-        console.log(tweets.statuses[0]);
-        tester = tweets.statuses
-        res.json(tester)
-    });
+const client = new Twitter(obj)
+
+
+router.get('/search', (req,res) => {
+  let query = req.query.q.trim()
+  client.get('search/tweets', {
+    q: query
+  }, function (error, tweets, response) {
+    let filtered = tweets.statuses.filter(tweet => {
+      if( tweet.entities.hashtags.includes(query) || !!tweet.text.match(new RegExp(`[\#|\@]${query} `, "i")) ) {
+        return true
+      }
+      else {
+        return false
+      }
+    })
+    res.json(filtered)
+  })
 })
 
 module.exports = router;
-
