b6568f28e047bf762694211695c7abf54c29c7f9
Nelson Parra <nlparra.dev@gmail.com>
2019-12-17 21:31:14 -0500

    create marker with all the trucks and object of the truck information

--- src/components/OtherMap.jsx
+++ src/components/OtherMap.jsx
@@ -1,19 +1,24 @@
 import {
     GoogleMapProvider,
-    HeatMap,
     InfoWindow,
     MapBox,
     Marker,
-    Polygon,
   } from '@googlemap-react/core'
   
  import React, { Component } from 'react'
+ import {CSSTransition} from'react-transition-group'
+ import trucks from '../data/trucks'
+
+ import './OtherMap.css'
  
  export default class OtherMap extends Component {
 
     state={
         longitude:-80.191788,
         latitude:25.761681,
+        show: 'none',
+        trucks: trucks,
+        truckIndx: 0
     }
     
     componentDidMount (){
@@ -23,37 +28,96 @@ import {
         })
     }
 
-
      render() {
+         let truck = this.state.trucks[this.state.truckIndx]
          return (
+             <React.Fragment>
+
+            {/* <CSSTransition
+                in={this.state.show}
+                mountOnEnter
+                unmountOnExit
+                timeout={200}
+                classNames="map-modal"
+            > */}
+            <div className="map-modal" style={{
+                width: '60%',
+                height: '70%',
+                backgroundColor: 'white',
+                boxShadow: '0 2px 8px rgba(0, 0, 0, 0.26)',
+                border:'hotpink solid 10px',
+                zIndex: '100',
+                position: 'fixed',
+                top: '14vh',
+                left: '22%',
+                display: this.state.show
+                
+            }}>
+                <button onClick={ e => {
+                    this.setState({show: 'none'})
+                }} className="btn btn-outline-danger btn-sm">X</button>
+                <h1>{truck.name}</h1>
+                
+            </div>
+                
+            {/* </CSSTransition> */}
+
            
                  <GoogleMapProvider>
+                    <div className="modal" style={{
+                        width: '300px',
+                        height: '300px',
+                        backgroundColor: 'red',
+
+                    }}>
+
+                    </div>
                    <MapBox
                      apiKey="AIzaSyB1Yrsp1FADOqPpMLzu43cJoPtwRb3LBxw"
                      opts={{
-                       center: {lat: this.state.latitude, lng: this.state.longitude},
-                       zoom: 14,
-                     }}
-                     style={{
-                       height: '80vh',
-                       width: '80vw',
-                     }}
-                     useDrawing
-                     useGeometry
-                     usePlaces
-                     useVisualization
-                     onCenterChanged={() => {
-                       console.log('The center of the map has changed.')
-                     }}
-                   />
+                         center: {lat: this.state.latitude, lng: this.state.longitude},
+                         zoom: 14,
+                        }}
+                        style={{
+                            height: '80vh',
+                            width: '80vw',
+                        }}
+                        useDrawing
+                        useGeometry
+                        usePlaces
+                        useVisualization
+                        onCenterChanged={() => {
+                            console.log('The center of the map has changed.')
+                        }}
+                        />
                    <Marker
                      id="marker"
                      opts={{
-                       draggable: true,
-                       label: 'hello',
-                       position: {lat: 39, lng: 116},
-                     }}
-                   />
+                         draggable: true,
+                         label: 'Here you are Fat ass!',
+                         position: {lat: this.state.latitude, lng: this.state.longitude},
+                        }}
+                        />
+                    
+                    {
+                        this.state.trucks.map( (t, index) => {
+                            return (
+                                <Marker
+                                key={index}
+                                 onClick={ () => {
+                                    this.setState({show:'block', truckIndx: index })
+                                 }}
+                                 id={t.id}
+                                 opts={{
+                                     draggable: true,
+                                     label:'food truck 1',
+                                     position: {lat: t.lat, lng: t.lng },
+                                 }}
+                                 />
+                            )
+                        })
+                    }
+                  
                    <InfoWindow
                      opts={{
                        content: 'This is an info window',
@@ -62,9 +126,17 @@ import {
                      visible
                    />
                  </GoogleMapProvider>
+            </React.Fragment>
+
                 )
                  
             
      }
  }
  
+// let trucks=[ {lat,lng,id}],
+
+
+// list with links
+
+// <link to=/truck/id >
---
+++ src/data/trucks.js
@@ -0,0 +1,8 @@
+const trucks = [
+    {lat: 25.819539, lng:-80.182389, id: 100, name: "A" },
+    {lat: 25.799449 , lng:-80.198997 , id: 101, name: "B"},
+    {lat:25.799218 , lng:-80.226463, id: 102, name: "C"},
+    {lat: 25.799456, lng: -80.203748, id: 103, name: "D"}
+]
+
+export default trucks
