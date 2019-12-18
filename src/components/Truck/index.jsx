5da052ca3923b016533f8c4350dcc9f327362ac9
Diego C <dcoro12@gmail.com>
2019-12-17 21:53:50 -0500

    implement Truck profile

---
+++ src/components/Truck/index.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import Map from '../Map'
+import "./index.css"
+
+
+export default class Truck extends Component {
+    state = {
+        name: "Zona Grill Venezuelan Food",
+        description: "Venezuelan",
+        phoneNumber: "786-930-9930",
+        website: "https://www.zonagrill.com",
+        startTime: "1800",
+        endTime: "2400",
+        hashtag: "ZonaGrill",
+        uploadedPic: "TruckImageLink"
+    }
+    render() {
+        return (
+            <div>
+                <Map/>
+
+                <div>
+                    <h2 className="truck-name">
+                        {this.state.name}
+                    </h2>
+
+
+                        <div className="truck-parent">
+                            <div className="truck-attributes"> 
+                                <label>Description</label>
+                                {this.state.description}
+                            </div>
+                            <div className="truck-attributes">      {this.state.phoneNumber}
+                            </div>
+                            <div className="truck-attributes">      {this.state.website}
+                            </div>
+                            <div className="truck-attributes">
+                                {this.state.startTime}
+                            </div>
+                            <div className="truck-attributes">
+                                {this.state.endTime}
+                            </div>
+                            <div className="truck-attributes">
+                                {this.state.hashtag}
+                            </div>
+                            <div className="truck-attributes">
+                                {this.state.uploadedPic}
+                        </div>
+                        </div>
+                </div>
+                <TwitterFeed/>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+// truck nam, truck description
+// phone number, website
+// Hours of Operation, Truk Hashtag
+// Upload a picture,
